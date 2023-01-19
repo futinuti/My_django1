3c7ff56bf995c36c5f368b7c6cf72643dfc8ab90
futinuti <amihailovp@gmail.com>
2023-01-19 13:10:43 +0200

    ClassWork

--- students/models.py
+++ students/models.py
@@ -1,20 +1,39 @@
 import datetime
 
+from django.core.validators import MinLengthValidator
 from django.db import models
+from faker import Faker
+
+from students.validators import validate_email_domain, ValidateEmailDomain
+
+VALID_DOMAINS = ('gmail.com', 'yahoo.com', 'test.com')
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50, verbose_name='First name', db_column='first name')
+    first_name = models.CharField(max_length=50, verbose_name='First name', db_column='first name',
+                                  validators=[MinLengthValidator(3)])
     last_name = models.CharField(max_length=50, verbose_name='Last name', db_column='last name')
     age = models.PositiveIntegerField()
     birthday = models.DateField(default=datetime.date.today)
     city = models.CharField(max_length=25, null=True, blank=True)
+    email = models.EmailField(validators=[ValidateEmailDomain(*VALID_DOMAINS)])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'students'
 
-
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name} {self.birthday} {self.email}'
+
+    @classmethod
+    def generate_fake_data(cls, count):
+        f = Faker()
+        for _ in range(count):
+            s = cls()
+            s.first_name = f.first_name()
+            s.last_name = f.last_name()
+            s.email = f'{s.first_name}.{s.last_name}@{f.random.choice(VALID_DOMAINS)}'
+            s.birthday = f.date_between(start_date='-65y', end_date='-18y')
+            s.age = f.random_int(min=18, max=65)
+            s.save()
--- students/views.py
+++ students/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Student
+
+
+# HttpRequest
+# HttpResponse
+
+
+def index(request):
+    students = Student.objects.all().order_by('birthday')
+    # string = '<br>'.join(str(student) for student in students)
+    string = '<table><thead><tr><th>First name</th><th>Last name</th><th>Email</th><th>Birthday</th></tr><thead><tbody>'
+    for st in students:
+        string += f'<tr><td>{st.first_name}</td><td>{st.last_name}</td><td>{st.email}</td><td>{st.birthday}</td></td>'
+    string += '</tbody></table>'
+    return HttpResponse(string)
+
+
