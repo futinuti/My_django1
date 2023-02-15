f9c4a4ffb3b22ce5d79835fcf1b5bbc496a310b3
futinuti <amihailovp@gmail.com>
2023-02-15 17:52:42 +0200

    CW_14

--- lms/urls.py
+++ lms/urls.py
@@ -1,36 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from groups.views import get_groups
-from groups.views import detail_group
-from groups.views import update_group
-from groups.views import create_group_view
-from students.views import get_students
-from students.views import delete_student
-from students.views import create_student_view
-from students.views import update_student
-from students.views import detail_student
-from students.views import index
-from teachers.views import get_teachers
-from teachers.views import create_teacher_view
-from teachers.views import update_teacher
-from teachers.views import detail_teacher
+from core.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('students/', get_students),
-    path('teachers/', get_teachers),
-    path('groups/', get_groups),
-    path('students/create/', create_student_view),
-    path('teachers/create/', create_teacher_view),
-    path('groups/create/', create_group_view),
-    path('students/update/<int:pk>/', update_student),
-    path('teachers/update/<int:pk>/', update_teacher),
-    path('groups/update/<int:pk>/', update_group),
-    path('students/detail/<int:pk>/', detail_student),
-    path('teachers/detail/<int:pk>/', detail_teacher),
-    path('groups/detail/<int:pk>/', detail_group),
-    path('students/delete/<int:pk>/', delete_student)
+    path('', index, name='home'),
+    path('students/', include('students.urls')),
 ]
--- students/views.py
+++ students/views.py
@@ -1,22 +1,11 @@
 from django.db.models import Q
 from django.http import HttpResponse,  HttpResponseRedirect
-from django.middleware.csrf import get_token
-from django.shortcuts import render
-# from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from webargs.djangoparser import use_args
 from webargs.fields import Str
-
 from .forms import CreateStudentForm, UpdateStudentForm
 from .models import Student
-# from .utils import format_list_students
-
-
-# HttpRequest
-# HttpResponse
-
-
-def index(request):
-    return render(request, 'students/index.html')
 
 
 @use_args(
@@ -29,19 +18,11 @@ def index(request):
 def get_students(request, args):
     students = Student.objects.all().order_by('birthday')
 
-    # if 'first_name' in args:
-    #     students = students.filter(first_name=args['first_name'])
-    #
-    # if 'last_name' in args:
-    #     students = students.filter(last_name=args['last_name'])
     if len(args) and (args.get('first_name') or args.get('last_name')):
         students = students.filter(
             Q(first_name=args.get('first_name', '')) | Q(last_name=args.get('last_name', ''))
         )
 
-    # string = form + format_list_students(students)
-    # response = HttpResponse(string)
-    # return response
     return render(
         request=request,
         template_name='students/list.html',
@@ -50,7 +31,7 @@ def get_students(request, args):
 
 
 def detail_student(request, pk):
-    student = Student.objects.get(pk=pk)
+    student = get_object_or_404(Student, pk=pk)
     return render(request, 'students/detail.html', {'title': 'Detail of student', 'student': student})
 
 
@@ -62,25 +43,25 @@ def create_student_view(request):
         form = CreateStudentForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/students/')
+            return HttpResponseRedirect(reverse('students:list'))
     return render(request, 'students/create.html', {'form': form})
 
 
 def update_student(request, pk):
-    student = Student.objects.get(pk=pk)
+    student = get_object_or_404(Student, pk=pk)
     if request.method == 'GET':
         form = UpdateStudentForm(instance=student)
     elif request.method == 'POST':
         form = UpdateStudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/students/')
+            return HttpResponseRedirect(reverse('students:list'))
     return render(request, 'students/update.html', {'form': form})
 
 
 def delete_student(request, pk):
-    st = Student.objects.get(pk=pk)
+    st = get_object_or_404(Student, pk=pk)
     if request.method == 'POST':
         st.delete()
-        return HttpResponseRedirect('/students/')
+        return HttpResponseRedirect(reverse('students:list'))
     return render(request, 'students/delete.html', {'student': st})
