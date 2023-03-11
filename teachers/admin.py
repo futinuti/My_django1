23e5785178986bdb674bc14d19530273ce281fef
futinuti <amihailovp@gmail.com>
2023-03-11 12:05:47 +0200

    move TeacherInLine to groups.admin

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,37 +1,6 @@
 from django.contrib import admin
 
-from groups.models import Group
 from teachers.models import Teacher
-
-
-# Register your models here.
-
-class TeachersInLine(admin.TabularInline):
-    model = Group.teachers.through
-
-    fields = ('get_first_name', 'get_last_name', 'get_salary')
-    exclude = ('teachers', )
-    extra = 0
-    readonly_fields = fields
-
-    def get_first_name(self, instance):
-        return f'{Teacher.objects.get(pk=instance.pk).get_first_name()}'
-
-    def get_last_name(self, instance):
-        return f'{Teacher.objects.get(pk=instance.pk).get_last_name()}'
-
-    def get_salary(self, instance):
-        return f'{Teacher.objects.get(pk=instance.pk).get_salary()}'
-
-    get_first_name.short_description = 'first_name'
-    get_last_name.short_description = 'last_name'
-    get_salary.short_description = 'salary'
-
-    def has_add_permission(self, request, obj):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 
 class TeacherAdmin(admin.ModelAdmin):
